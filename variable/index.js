a4012524c7fcc44be218a58ec3a331dd7e44a6b2
YWQ1998 <3511350954@qq.com>
2021-12-11 15:30:04 +0800

    变量

---
+++ variable/index.js
@@ -0,0 +1,13 @@
+// var age;
+// age = 13;
+// console.log('声明变量并赋值', age);
+
+// var myName = '小明';
+// console.log(myName)
+// myName = '李四';
+// console.log(myName);
+
+var sname = '小明',
+  age = '23',
+  gz = '2000';
+console.log(sname + '的年龄是：' + age + '，他的工资是： ' + gz);
